df91f38a8bfa73295fdf6c0f9e5df8e237edbaae
chenjing <chenjing@autohome.com.cn>
2019-01-21 14:34:36 +0800

    BulkInitVideoVV

--- updateEs/BulkInitVideoVV.py
+++ updateEs/BulkInitVideoVV.py
@@ -5,47 +5,55 @@ import sys
 
 from elasticsearch import helpers, Elasticsearch, ConnectionTimeout
 
-def main():
-    s = "http://10.168.99.124:9200/"
-    sindex = "zxpt_content_alias"
-    size = 1000
-    fieldName="vvCount"
-    defaultFieldName="viewCount"
 
-    export(s, sindex, size,fieldName,defaultFieldName)
+def main():
+    s = ["http://10.168.99.124:9200", "http://10.168.99.122:9200", "http://10.168.99.83:9200"]
+    sindex = "zxpt_content_alias"
+    size = 5000
+    fieldName = "vvCount"
+    defaultFieldName = "viewCount"
+
+    export(s, sindex, size, fieldName, defaultFieldName)
 
     print "[Done]"
 
 
-def export(s, sindex, size,fieldName,defaultFieldName):
-    es = Elasticsearch([s])
-    page = es.search(index=sindex, scroll='2m', size=size,body={
+def export(s, sindex, size, fieldName, defaultFieldName):
+    es = Elasticsearch(s)
+    page = es.search(index=sindex, scroll='5m', size=size, body={
         "query": {
-    "bool": {
-      "must": [
-        {
-          "range": {
-            "bizType": {
-              "gte": 40,
-              "lte": 50
+            "bool": {
+                "must": [
+                    {
+                        "term": {
+                            "bizType": {
+                                "value": "41"
+                            }
+                        }
+                    },
+                    {
+                        "range": {
+                            "bizId": {
+                                "gte": 420000,
+                                "lte": 430000
+                            }
+                        }
+                    }
+                ],
+                "must_not": {
+                    "exists": {
+                        "field": "vvCount"
+                    }
+                }
             }
-          }
-        }
-      ],
-      "must_not": {
-        "exists": {
-          "field": "vvCount"
-        }
-      }
-    }
-  },
-  "sort": [
-    {
-      "bizId": {
-        "order": "asc"
-      }
-    }
-  ]
+        },
+        "sort": [
+            {
+                "bizId": {
+                    "order": "asc"
+                }
+            }
+        ]
     })
 
     try:
@@ -59,7 +67,7 @@ def export(s, sindex, size,fieldName,defaultFieldName):
 
     allsize = len(page['hits']['hits'])
 
-    bulkdata(es, page['hits']['hits'], size)
+    bulkdata(es, page['hits']['hits'], size, fieldName, defaultFieldName)
 
     while scroll_size > 0:
         printSth("Scrolling...")
@@ -72,27 +80,28 @@ def export(s, sindex, size,fieldName,defaultFieldName):
         allsize += scroll_size
         printSth("scroll size: " + str(scroll_size) + "  all size: " + str(allsize) + "  \r\n")
 
-        bulkdata(es, page['hits']['hits'],size)
+        bulkdata(es, page['hits']['hits'], size, fieldName, defaultFieldName)
 
     print("\n" + sid)
 
     es.clear_scroll(sid)
 
-def bulkdata(es_des, pagedata, size):
+
+def bulkdata(es_des, pagedata, size, fieldName, defaultFieldName):
     f = pagedata
     for a in f:
-        a["_source"]["vvCount"] = a["_source"]["viewCount"]
+        if((defaultFieldName) in a["_source"].keys()):
+            a["_source"][fieldName] = a["_source"][defaultFieldName + "a"]
     try:
         helpers.bulk(es_des, f)
     except  ConnectionTimeout as error:
         printSth("time out retry\r\n")
-        bulkdata(es_des, pagedata, size)
-
+        bulkdata(es_des, pagedata, size,fieldName,defaultFieldName)
 
 
 def getpage(es, sid):
     try:
-        page = es.scroll(scroll_id=sid, scroll='2m')
+        page = es.scroll(scroll_id=sid, scroll='5m')
 
         if page is None:
             print "page None try again:"
